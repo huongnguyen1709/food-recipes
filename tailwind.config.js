7c692fe214171d9dd85980808122fd96d3bbd9fd
Huong <huongnguyen.nth1709@gmail.com>
2023-07-05 13:49:33 +0300

    add custom colors and fontFamily

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        body: ['Nunito'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
